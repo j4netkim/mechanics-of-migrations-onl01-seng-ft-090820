6c09b88d5900665f442049ff35f48c98d7fd0a51
J K <jkim65@buffalo.edu>
2020-10-12 22:13:37 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -4,7 +4,8 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     create_table :artists do |t|
       t.string :name
       t.string :genre
-      t.integer :age 
+      t.integer :age
       t.string :hometown
+    end
   end
 end
